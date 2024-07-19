c8c2a86835b2dad6f06c629cd93f98e7bb38e6db
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-07-20 05:19:13 +0530

    base-site

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import '@mantine/core/styles.css';
-import { MantineProvider, Center } from '@mantine/core';
+import { MantineProvider, Grid, Button, Switch } from '@mantine/core';
+
+const mode = () => {
+  console.log("yes")
+}
 
 function App() {
   return (
     <MantineProvider>
-      <Center>
-        <h1>Coming Soon</h1>
-      </Center>
+      <div className="center">
+        <div className="container">
+          <h1><span className="head">Zap</span> <br /><span className="sub">Media</span></h1>
+          <span className='mini-sub'>Open Source digital media services solo developed by <a href="https://natya.is-a.dev">NatyaCodes</a></span>
+          <Grid className='btn-grid'>
+            <Grid.Col span={4}>
+              <Button className='home-btn'>Zap Social</Button>
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <Button className='home-btn'>Zap Blogs</Button>
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <Button className='home-btn'>ZapFolio</Button>
+            </Grid.Col>
+          </Grid>
+        </div>
+        <Switch className='dark-switch' size="lg" onLabel="Dark" offLabel="Light" onClick={mode} />
+      </div>
     </MantineProvider>
   )
 }
