bf26cfccb1f439e542b2fd9d8f3a4a8bb37b2142
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-07-20 04:16:14 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import '@mantine/core/styles.css';
+import { MantineProvider, Center } from '@mantine/core';
+
+function App() {
+  return (
+    <MantineProvider>
+      <Center>
+        <h1>Coming Soon</h1>
+      </Center>
+    </MantineProvider>
+  )
+}
+
+export default App
